2c2c5a20bc2e966005165cb27e03a6a3af765a06
alihtt <aliht.workspace@gmail.com>
2024-11-16 01:54:11 +0330

    changed mappings and options of nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -5,6 +5,9 @@ require("nvchad.mappings")
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
-map("i", "jk", "<ESC>")
+map("i", "jj", "<ESC>")
+
+map("v", "<S-j>", ":m '>+1<CR>gv=gv", { noremap = true, silent = true })
+map("v", "<S-k>", ":m '<-2<CR>gv=gv", { noremap = true, silent = true })
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -7,6 +7,14 @@ o.shiftwidth = 4
 o.tabstop = 4
 o.softtabstop = 4
 
+o.number = true
+o.relativenumber = true
+o.clipboard = "unnamedplus"
+o.ignorecase = true
+o.smartcase = true
+o.cursorline = true
+o.scrolloff = 10
+
 -- o.cursorlineopt ='both' -- to enable cursorline!
 
 -- set filetype for .CBL COBOL files.
